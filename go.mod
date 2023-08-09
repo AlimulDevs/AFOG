0300c62cc04cbc50365eecfc6f3c6dc949d88f24
alimul <alimuldev@gmail.com>
2023-08-09 19:39:44 +0700

    add

---
+++ go.mod
@@ -0,0 +1,3 @@
+module AFOG
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Mahasiswa struct {
+	Nama string
+	Nim  string
+	Umur int
+}
+
+func main() {
+	mahasiswa := Mahasiswa{"Alice", "12345", 21}
+	fmt.Printf("Mahasiswa: %s, Nim: %s, Umur: %d\n", mahasiswa.Nama, mahasiswa.Nim, mahasiswa.Umur)
+
+	angka := []int{1, 2, 3, 4, 5}
+	for _, nilai := range angka {
+		fmt.Println("Angka:", nilai)
+	}
+}
